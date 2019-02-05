2a860e29ede02d2c86963abdd78cc27223cc0f66
Travis Grammer <travisjgrammer@gmail.com>
2019-02-04 23:39:11 -0500

    use math/rand but frame for crypto/rand

--- util.go
+++ util.go
@@ -1,13 +1,24 @@
 package dice
 
 import (
-	mathrand "math/rand"
+	crand "crypto/rand"
+	"math/big"
+	rand "math/rand"
 	"time"
 )
 
 func init() {
-	// seed PRNG
-	mathrand.Seed(time.Now().UTC().UnixNano())
+	// seed PRNG with crypto-secure integer
+	// rand.Seed(cryptoSeed())
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func cryptoSeed() int64 {
+	seed, err := crand.Int(crand.Reader, big.NewInt(int64(^uint64(0)>>1)))
+	if err != nil {
+		panic(err)
+	}
+	return seed.Int64()
 }
 
 func quote(s string) string {
