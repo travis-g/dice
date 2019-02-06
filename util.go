b9091a79f507b0cada07c24c10dd11c14d28a2ec
Travis Grammer <travisjgrammer@gmail.com>
2019-02-05 20:36:29 -0500

    add seed function

--- util.go
+++ util.go
@@ -8,18 +8,24 @@ import (
 )
 
 func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	// seed PRNG with crypto-secure integer
-	// rand.Seed(cryptoSeed())
+	// seed math/rand with the time by default
+	Seed(time.Now().UTC().UnixNano())
 }
 
-func cryptoSeed() int64 {
-	seed, err := crand.Int(crand.Reader, big.NewInt(int64(^uint64(0)>>1)))
+// Seed is a convenience function to re-seed math/rand.
+func Seed(seed int64) {
+	rand.Seed(seed)
+}
+
+// CryptoInt64 is a convenience function that returns a cryptographically random
+// int64. If there is a problem generating enough entropy it will return a
+// non-nil error.
+func CryptoInt64() (int64, error) {
+	i, err := crand.Int(crand.Reader, big.NewInt(int64(^uint64(0)>>1)))
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-	return seed.Int64()
+	return i.Int64(), nil
 }
 
 func quote(s string) string {
