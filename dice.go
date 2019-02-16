1380a796e2efb1f04a4b141c709f85c9a0574b14
Travis Grammer <travisjgrammer@gmail.com>
2019-02-15 20:43:01 -0500

    use dropkeep regex and named fields

--- dice.go
+++ dice.go
@@ -22,18 +22,26 @@ func Parse(notation string) (RollableSet, error) {
 
 // parse is the real-deal notation parsing method.
 func parse(notation string) (RollableSet, error) {
-	matches := DiceNotationRegex.FindStringSubmatch(notation)
+	matches := DropKeepNotationRegex.FindStringSubmatch(notation)
 	if len(matches) < 3 {
 		return nil, &ErrParseError{notation, notation, "", ": failed to identify dice components"}
 	}
 
+	// extract named capture groups to map
+	components := make(map[string]string)
+	for i, name := range DropKeepNotationRegex.SubexpNames() {
+		if i != 0 && name != "" {
+			components[name] = matches[i]
+		}
+	}
+
 	// Parse and cast dice properties from regex capture values
-	count, err := strconv.ParseUint(matches[1], 10, 0)
+	count, err := strconv.ParseUint(components["count"], 10, 0)
 	if err != nil {
 		// either there was an implied count, ex 'd20', or count was invalid
 		count = 1
 	}
-	size, err := strconv.ParseUint(matches[2], 10, 0)
+	size, err := strconv.ParseUint(components["size"], 10, 0)
 	if err == nil {
 		// valid size, so build the dice set
 		set := NewDieSet(int(size), uint(count))
@@ -41,11 +49,11 @@ func parse(notation string) (RollableSet, error) {
 	}
 
 	// Check for special dice types
-	if matches[2] == "F" {
+	if components["size"] == "F" {
 		set := NewFateDieSet(uint(count))
 		return &set, nil
 	}
 
 	// Couldn't parse the "size" as a uint and it's not a special die type
-	return &DieSet{}, &ErrParseError{notation, matches[2], "size", ": invalid size"}
+	return &DieSet{}, &ErrParseError{notation, components["size"], "size", ": invalid size"}
 }
