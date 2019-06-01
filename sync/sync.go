e3eb9fdd782a4ff7112bcbfa1a6fb3b626127a7c
Travis Grammer <travisjgrammer@gmail.com>
2019-06-01 14:45:23 -0400

    RWLockerRoller interface

--- sync/sync.go
+++ sync/sync.go
@@ -1,3 +1,6 @@
+/*
+Package sync implements a thread-safe wrapper for rollable dice.
+*/
 package sync
 
 import (
@@ -7,35 +10,47 @@ import (
 	"github.com/travis-g/dice"
 )
 
-// RWMutexRoller is a Roller wrapped with a sync.RWMutex.
+// RWLockerRoller is implemented by any value that implements dice.Roller,
+// sync.Locker, and has an RLock and RUnlock method.
+type RWLockerRoller interface {
+	dice.Roller
+	sync.Locker
+	RLock()
+	RUnlock()
+}
+
+// RWMutexRoller is a dice.Roller wrapped with a sync.RWMutex. The methods of
+// RWMutexRoller call the embedded Roller's methods within a thread-safe
+// context.
 type RWMutexRoller struct {
 	l   sync.RWMutex
 	die dice.Roller
 }
 
-// Wrap creates an RWMutexRoller by wrapping a Roller with a sync.RWMutex.
+// Wrap creates an RWMutexRoller out of a Roller by wrapping it with a
+// sync.RWMutex.
 func Wrap(die dice.Roller) *RWMutexRoller {
 	return &RWMutexRoller{
 		die: die,
 	}
 }
 
-// Roll rolls the embedded Roller with thread safety. Rollers can call their
-// Roll() function to calculate their result with thread safety.
+// Roll rolls the embedded Roller with thread safety. It calls the embedded
+// Roller's Roll() method to calculate its result.
 func (r *RWMutexRoller) Roll(ctx context.Context) error {
 	r.l.Lock()
 	defer r.l.Unlock()
 	return r.die.Roll(ctx)
 }
 
-// Reroll rolls the embedded Roller with thread safety.
+// Reroll re-rolls the embedded Roller with thread safety.
 func (r *RWMutexRoller) Reroll(ctx context.Context) error {
 	r.l.Lock()
 	defer r.l.Unlock()
 	return r.die.Reroll(ctx)
 }
 
-// Total read-locks the embedded Roller and returns the total.
+// Total read-locks the embedded Roller and returns its total.
 func (r *RWMutexRoller) Total(ctx context.Context) (float64, error) {
 	r.l.RLock()
 	defer r.l.RUnlock()
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,19 +1,5 @@
 package sync
 
-import (
-	"sync"
-
-	"github.com/travis-g/dice"
-)
-
-type rwMutexer interface {
-	sync.Locker
-	RLock()
-	RUnlock()
-}
-
-// ensure RWMutexRoller can be implemented like an RWMutex for thread safety
-var _ = rwMutexer(&RWMutexRoller{})
-
-// ensure RWMutexRoller implements Roller
-var _ dice.Roller = (*RWMutexRoller)(nil)
+// ensure RWMutexRoller can be implemented like a Roller and an RWMutex for
+// thread safety
+var _ = RWLockerRoller(&RWMutexRoller{})
