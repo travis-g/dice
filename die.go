273db3923e20aa75643cb9030cda982a51b037b2
Travis Grammer <travisjgrammer@gmail.com>
2025-08-09 10:46:44 -0400

    chore: combine die and die_type files

--- die.go
+++ die.go
@@ -7,6 +7,31 @@ import (
 	"sync/atomic"
 )
 
+// DieType is the enum of types that a die or dice can be
+type DieType string
+
+// Types of dice/dice groups.
+const (
+	// Concrete dice types: these can be used to instantiate a new rollable.
+	TypePolyhedron DieType = ""
+	TypeFudge      DieType = "fudge"
+
+	// Meta dice types: these are used to classify rollable groups and unknown
+	// dice.
+	TypeUnknown DieType = "unknown"
+)
+
+func (t DieType) String() string {
+	switch t {
+	case TypePolyhedron:
+		return "polyhedron"
+	case TypeFudge:
+		return "fudge"
+	default:
+		return "unknown"
+	}
+}
+
 // Die represents an internally-typed die. If Result is a non-nil pointer, it
 // is considered rolled.
 type Die struct {
